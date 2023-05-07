6bba71248e3d33af7ae5262ae589a3cd062ead7c
anupzpt <prajapatianu433@gmail.com>
2023-05-07 11:01:28 +0545

    crud operation

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,18 @@ public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         String url ="jdbc:mysql://localhost/mydatabase";
-        
+
         //connect to database
         Connection conn= DriverManager.getConnection(url,"root","");
         System.out.println("connection success");
 
         //fetch data from database
         Statement statement =conn.createStatement();
+//      statement.executeUpdate("insert into data values ('7','Neha','Ktm')");
+        statement.executeUpdate("delete from data where id=6");
+        statement.executeUpdate("update data set address='BKT' where id=3");
         ResultSet resultSet=statement.executeQuery("Select * from data");
+
         System.out.println(resultSet);
 
         while (resultSet.next())
